1f0077323baaf89d80c780b54db233211a4a8876
Jonathan Sieber <jonathan.sieber@hicknhack-software.com>
2019-05-03 18:31:03 +0200

    TemplateOfTypes indexOf implementation

--- src/meta17.lib/meta17/TemplateOfTypes.indexOf.h
+++ src/meta17.lib/meta17/TemplateOfTypes.indexOf.h
@@ -1,9 +1,18 @@
 #pragma once
+#include "IndexPack.for.h"
 #include "Type.h"
 
 #include "Type.ops.h" // type == type
 
 namespace meta17 {
+
+/// return number of occurences of type T in the TypePack
+template<class T, template<typename...> class Template, class... Ts>
+constexpr auto templateCountOf(Type<Template<Ts...>>, Type<T> = {}) -> size_t {
+    return ((type<Ts> == type<T> ? 1 : 0) + ... + 0);
+}
+template<class T, class Template>
+constexpr auto template_count_of = templateCountOf(type<Template>, type<T>);
 
 template<class T, template<typename...> class Template, class... Ts>
 constexpr auto templateContains(Type<Template<Ts...>>, Type<T> = {}) -> bool {
@@ -11,5 +20,20 @@ constexpr auto templateContains(Type<Template<Ts...>>, Type<T> = {}) -> bool {
 }
 template<class T, class Template>
 constexpr auto template_contains = templateContains<T>(type<Template>);
+
+template<class T, template<typename...> class Template, class... Ts, size_t... Is>
+constexpr auto templateIndexOf(Type<Template<Ts...>>, IndexPack<Is...>, Type<T> = {}) -> size_t {
+    using TP = TypePack<Ts...>;
+    return 1 == templateCountOf<T>(type<TP>) ? (((type<Ts> == type<T>) ? Is : 0) + ...) : sizeof...(Ts);
+}
+
+template<class T, template<typename...> class Template, class... Ts>
+constexpr auto templateIndexOf(Type<Template<Ts...>>, Type<T> = {}) {
+    constexpr auto ip = indexPackFor<Ts...>();
+    return templateIndexOf<T>(type<Template<Ts...>>, ip);
+}
+
+template<class T, class Template>
+constexpr auto template_index_of = templateIndexOf(type<Template>, type<T>);
 
 } // namespace meta17
--- src/meta17.lib/meta17/TemplateOfTypes.indexOf.test.cpp
+++ src/meta17.lib/meta17/TemplateOfTypes.indexOf.test.cpp
@@ -5,3 +5,12 @@
 using namespace meta17;
 
 static_assert(template_contains<int, TypePack<int, char>>);
+
+static_assert(templateCountOf<int>(type<TypePack<int, char, int>>) == 2);
+static_assert(template_count_of<int, TypePack<int, char, int>> == 2);
+
+static_assert(template_index_of<int, TypePack<char, int, unsigned>> == 1);
+static_assert(template_index_of<int, TypePack<float, double>> == 2);
+
+// static_assert(checkedIndexOf<int>(type_pack<void, char, int>) == 2);
+// static_assert(checked_index_of<int, TypePack<void, char, int>> == 2);
