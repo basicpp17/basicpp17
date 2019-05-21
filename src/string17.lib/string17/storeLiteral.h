e5e5307c4399f7ef239de0617c5a5bd7f0a49657
Jonathan Sieber <jonathan.sieber@hicknhack-software.com>
2019-05-21 16:10:30 +0200

    For loop with unsigned literals (fix signedness warning of clang)

--- src/string17.lib/string17/storeLiteral.h
+++ src/string17.lib/string17/storeLiteral.h
@@ -6,7 +6,7 @@ namespace string17 {
 template<size_t N>
 constexpr auto storeLiteral(const StringArray<N>& o) noexcept -> StringStore<N - 1> {
     auto r = StringStore<N - 1>{};
-    for (auto i = 0; i < N - 1; i++) r.data[i] = o[i];
+    for (auto i = 0U; i < N - 1; i++) r.data[i] = o[i];
     return r;
 }
 
--- src/string17.lib/string17/storeView.h
+++ src/string17.lib/string17/storeView.h
@@ -7,7 +7,7 @@ namespace string17 {
 template<size_t N>
 constexpr auto storeView(const StringView& o) noexcept -> StringStore<N> {
     auto r = StringStore<N>{};
-    auto i = 0;
+    auto i = 0U;
     for (; i < o.size(); i++) r.data[i] = o[i];
     for (; i < N; i++) r.data[i] = '\0';
     return r;
