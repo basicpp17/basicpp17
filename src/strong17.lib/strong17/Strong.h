5e95a97db1ba7e6e9133d04d76f0bdf1510a838d
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2019-07-21 21:04:14 +0200

    relaxed strong tests a bit

--- src/strong17.lib/strong17/Strong.h
+++ src/strong17.lib/strong17/Strong.h
@@ -8,6 +8,8 @@ namespace strong17 {
 template<class T, class = void>
 struct IsValidTag : std::true_type {};
 template<class T>
+struct IsValidTag<T, std::enable_if_t<std::is_enum_v<T>>> : std::true_type {};
+template<class T>
 struct IsValidTag<T, std::enable_if_t<!std::is_class_v<T>>> : std::false_type {};
 template<class T>
 struct IsValidTag<T, std::enable_if_t<std::is_class_v<T> && (sizeof(T) == sizeof(T))>> : std::is_empty<T> {};
@@ -16,7 +18,7 @@ template<class V, class... Tags>
 constexpr auto checkStrongArgs() {
     if constexpr (is_strong<V>)
         return META17_DEFER_STATIC_ERROR("Nested Strong is not allowed!");
-    else if constexpr (IsValidTag<V>{})
+    else if constexpr (!std::is_object_v<V>)
         return META17_DEFER_STATIC_ERROR("Strong value should store something!");
     else if constexpr (sizeof...(Tags) < 1)
         return META17_DEFER_STATIC_ERROR("Strong requires at least one tag!");
