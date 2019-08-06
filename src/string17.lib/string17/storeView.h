5182ae79a8359ab770474c1f933c6289fabf593d
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2019-08-06 22:14:42 +0200

    fixed constexpr for string operations

--- src/string17.lib/string17/storeView.h
+++ src/string17.lib/string17/storeView.h
@@ -8,7 +8,7 @@ template<size_t N>
 constexpr auto storeView(const StringView& o) noexcept -> StringStore<N> {
     auto r = StringStore<N>{};
     auto i = 0U;
-    for (; i < o.size(); i++) r.data[i] = o[i];
+    for (; i < N && i < o.size(); i++) r.data[i] = o[i];
     for (; i < N; i++) r.data[i] = '\0';
     return r;
 }
--- src/string17.lib/string17/viewStore.h
+++ src/string17.lib/string17/viewStore.h
@@ -6,8 +6,7 @@ namespace string17 {
 
 template<size_t N>
 constexpr auto viewStore(const StringStore<N>& store) -> StringView {
-    auto ptr = static_cast<const char*>(store.data);
-    return {ptr, ptr + N};
+    return {static_cast<const char*>(store.data), static_cast<const char*>(store.data) + N};
 }
 
 } // namespace string17
